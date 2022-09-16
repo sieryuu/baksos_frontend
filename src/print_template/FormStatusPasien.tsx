075c0a2c3a9388db06ca961168e8f2c6f50bc7d5
Sieryuu <sieryuu@gmail.com>
2022-09-16 15:58:24 +0700

    Add Pendamping in FormStatusPasien

--- src/print_template/FormStatusPasien.tsx
+++ src/print_template/FormStatusPasien.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
 
 interface FormStatusPasienProps {
-  pasien: PasienType;
+    pasien: PasienType;
 }
 
 export default class FormStatusPasien extends React.PureComponent<FormStatusPasienProps> {
-  render() {
-    const { pasien } = this.props;
-    return (
-      <div style={{ fontSize: '15px' }}>
-        <table style={{ width: '100%' }}>
-          <tr style={{ verticalAlign: 'top', height: '150px' }}>
-            <th style={{ width: '16%' }}></th>
-            <th style={{ width: '40%' }}> </th>
-            <th style={{ width: '35%' }}> </th>
-            <th style={{ width: '9%' }}> </th>
-          </tr>
-          <tr style={{ height: '25px' }}>
-            <td></td>
-            <td colSpan={2}>{pasien.nama}</td>
-          </tr>
-          <tr style={{ height: '25px' }}>
-            <td></td>
-            <td valign="top" colSpan={2}>
-              {pasien.alamat.replace(/(\r\n|\n|\r)/gm, ' ')}
-            </td>
-          </tr>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        const { pasien } = this.props;
+        return (
+            <div style={{ fontSize: '15px' }}>
+                <table style={{ width: '100%' }}>
+                    <tr style={{ verticalAlign: 'top', height: '100px' }}>
+                        <th style={{ width: '16%' }}></th>
+                        <th style={{ width: '40%' }}> </th>
+                        <th style={{ width: '40%' }}> </th>
+                        <th style={{ width: '20%' }}> </th>
+                    </tr>
+                    <tr style={{ height: "50px" }}>
+                        <td></td>
+                        <td></td>
+                        <td>Pendamping: {pasien.nama_keluarga} ({pasien.nomor_telepon_keluarga})</td>
+                        <td></td>
+                    </tr>
+                    <tr style={{ height: '25px' }}>
+                        <td></td>
+                        <td colSpan={2}>{pasien.nama}</td>
+                    </tr>
+                    <tr style={{ height: '20px' }}>
+                        <td></td>
+                        <td valign="top" colSpan={2}>
+                            {pasien.alamat.replace(/(\r\n|\n|\r)/gm, ' ')}
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
 }
